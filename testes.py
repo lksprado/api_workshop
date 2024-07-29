6fd077895acdf4b2a1e5a42bb8ac744b1fa02d80
lksprado <lks-prado@live.com>
2024-07-29 10:17:26 -0300

    new branch and tests

--- testes.py
+++ testes.py
@@ -7,13 +7,20 @@ from main import app
 client = TestClient(app)
 
 
-def test_ola_mundo():
+def test_ola_mundo_status_code():
     response =  client.get("/")
     assert response.status_code == 200 
-    
-    
     
 def test_ola_mundo_json():
     response = client.get("/")
     assert response.json() == {"Olá":"Mundo"}
     
+
+def test_listar_produtos_status_code():
+    response =  client.get("/produtos")
+    assert response.status_code == 200 
+    
+
+def test_tamanho_lista_produtos():
+    response = client.get("/produtos")
+    assert len(response.json()) == 5     
